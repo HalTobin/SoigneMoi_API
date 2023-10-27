e35e1e993a3d397a53c255cf6f0777ca3e29d7af
Alexis Aneas <alexis.aneas@outlook.fr>
2023-10-27 10:25:16 +0200

    first commit

---
+++ src/main/java/com/soignemoi/soignemoiapi/SoignemoiapiApplication.java
@@ -0,0 +1,13 @@
+package com.soignemoi.soignemoiapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoignemoiapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoignemoiapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/soignemoi/soignemoiapi/SoignemoiapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.soignemoi.soignemoiapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoignemoiapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
